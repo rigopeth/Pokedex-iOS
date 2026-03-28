01cc564233e09cf8c07ec83c3a899028c792a970
Rigoberto <RDominguez@intercambioexpress.com>
2026-03-28 12:55:18 -0600

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Rigoberto Dominguez Garcia on 28/03/26.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
